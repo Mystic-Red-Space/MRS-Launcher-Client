c9eca2a5709ea44d298dd44d095612244cf3837c
Lapis <lapis0875@gmail.com>
2019-02-24 14:29:55 +0900

    Update GamePatch.cs
    Add shaderpacks folder in GamePatch blacklist

--- MRSLauncherClient/Core/GamePatch.cs
+++ MRSLauncherClient/Core/GamePatch.cs
@@ -50,6 +50,7 @@ namespace MRSLauncherClient
                     "versions",
                     "screenshots",
                     "saves",
+                    "shaderpacks",
                     "launcher"
                 };
 
