b88b5b0b6978207c7852cba41bf2af51254e9542
Lapis <lapis0875@gmail.com>
2019-03-02 03:34:05 +0900

    Working on SettingsPage.xaml(.cs)
    Working on RAM setting. Found error which occurs when txtRam.Text contains something that's type is not numeric.

--- MRSLauncherClient/UI/SettingsPage.xaml.cs
+++ MRSLauncherClient/UI/SettingsPage.xaml.cs
@@ -34,8 +34,40 @@ namespace MRSLauncherClient
         {
             int ramValue = (int)ramSlider.Value;
             txtRam.Text = ramValue.ToString();
-            lvRamViewer.Content = (ramValue + "MB / " + ramSlider.Maximum+"MB");
-            Setting.Json.MaxRamMb = (int)ramSlider.Value;
+            lvRamViewer.Content = (txtRam.Text+"MB / "+ramSlider.Maximum+"MB");
+        }
+        
+        private void TxtRam_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtRam.Clear();
+        }
+
+        private void TxtRam_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (int.Parse(txtRam.Text) > ramSlider.Maximum || int.Parse(txtRam.Text) < ramSlider.Minimum)
+                {
+                    txtRam.Text = ramSlider.Maximum.ToString();
+                    TxtRam_LostFocus(sender,e);
+                    return;
+                }
+                else{
+                    ramSlider.Value = int.Parse(txtRam.Text);
+                    lvRamViewer.Content = (ramSlider.Value + "MB / " + ramSlider.Maximum + "MB");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse '{txtRam.Text}'");
+            }
+        }
+
+        private void TxtRam_LostFocus(object sender, RoutedEventArgs e)
+        {
+            int ramValue = int.Parse(txtRam.Text);
+            txtRam.Text = ramValue.ToString();
+            lvRamViewer.Content = (txtRam.Text + "MB / " + ramSlider.Maximum + "MB");
         }
 
         private void Settings_Loaded(object sender, RoutedEventArgs e)
@@ -56,5 +88,13 @@ namespace MRSLauncherClient
             Setting.Json.UseCustomJVM = cbCustomJVM.IsChecked ?? false; // 만약 값이 null 이라면 false 를 반환
         }
 
+        private void CbCustomJVM_Checked(object sender, RoutedEventArgs e)
+        {
+            cbCustomJVM.Content = "enabled";
+        }
+        private void CbCustomJVM_Unchecked(object sender, RoutedEventArgs e)
+        {
+            cbCustomJVM.Content = "disabled";
+        }
     }
 }
