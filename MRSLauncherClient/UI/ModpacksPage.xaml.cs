777a0c3b3a501d5e816d11ec80650fd9ff4151f0
알파 <ksi123456ab@naver.com>
2019-06-01 23:56:45 +0900

    grid modpackpage

--- MRSLauncherClient/UI/ModpacksPage.xaml.cs
+++ MRSLauncherClient/UI/ModpacksPage.xaml.cs
@@ -19,12 +19,38 @@ namespace MRSLauncherClient
         }
 
         MSession Session;
-        bool IsPackLoaded = false;
+        bool IsPackLoading = false;
 
         private void Modpacks_Loaded(object sender, RoutedEventArgs e) // 모드팩 로딩
         {
-            if (IsPackLoaded) return;
-            IsPackLoaded = true;
+            LoadPackes();
+        }
+
+        private void BtnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            LoadPackes();
+        }
+
+        private void Control_Click(object sender, EventArgs e) // 모드팩 클릭했을때
+        {
+            var control = (ModPackControl)sender;
+            var page = new ModpackInfoPage(control.ModPack, Session);
+            page.PageReturned += Page_PageReturned;
+            frmContent.Content = page;
+        }
+
+        private void Page_PageReturned(object sender, EventArgs e)
+        {
+            frmContent.Content = null;
+        }
+
+        private void LoadPackes()
+        {
+            if (IsPackLoading) return;
+            IsPackLoading = true;
+
+            stPacks.Children.Clear();
+            lvLoading.Visibility = Visibility.Visible;
 
             var th = new Thread(new ThreadStart(delegate
             {
@@ -54,23 +80,11 @@ namespace MRSLauncherClient
                         }
 
                         lvLoading.Visibility = Visibility.Collapsed;
+                        IsPackLoading = false;
                     }
                 }));
             }));
             th.Start();
-        }
-
-        private void Control_Click(object sender, EventArgs e) // 모드팩 클릭했을때
-        {
-            var control = (ModPackControl)sender;
-            var page = new ModpackInfoPage(control.ModPack, Session);
-            page.PageReturned += Page_PageReturned;
-            frmContent.Content = page;
-        }
-
-        private void Page_PageReturned(object sender, EventArgs e)
-        {
-            frmContent.Content = null;
         }
     }
 }
